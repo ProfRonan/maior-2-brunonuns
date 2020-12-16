f37b9748de01c5f955201c356626725052df8c1b
Bruno Nunes <brunonuns@alu.ufc.br>
2020-12-16 18:26:02 +0000

    Atividade do maior número feita.

--- main.py
+++ main.py
@@ -4,6 +4,13 @@
 def main():
     """Função principal que será rodada quando o script for passado para o interpretador."""
     # COLOQUE SEU CÓDIGO AQUI
+    num_1 = float(input('Digite um número: '))
+    num_2 = float(input('Digite outro número: '))
+    if (num_1 >= num_2):
+      maior = num_1
+    else:
+      maior = num_2
+    print(f'{maior}')
 
 if __name__ == '__main__':
     main()
